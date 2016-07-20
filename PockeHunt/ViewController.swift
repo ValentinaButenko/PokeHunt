b81ea2f0e91ef40e0e5028e529166fec56423273
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-07-20 14:51:02 +0300

    Add basic project

---
+++ PockeHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PockeHunt
+//
+//  Created by Dmitry Fa[n]tastik on 20/07/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
