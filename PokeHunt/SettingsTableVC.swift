9ecc7ba5475a50f8bd086e5d2bb81645a85da9bf
Valentina <v.o.butenko@gmail.com>
2016-07-23 10:21:07 +0300

    Basic table View
    Former-commit-id: e68fd396614b5765585c9b4946e83ff0798b0f9d

---
+++ PokeHunt/SettingsTableVC.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsTableVC.swift
+//  PokeHunt
+//
+//  Created by Valentina Butenko on 7/23/16.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+           }
+
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
+    }
+
+   }
