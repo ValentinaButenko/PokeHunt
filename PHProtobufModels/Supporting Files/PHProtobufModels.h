543c2eadb31a025824453270bebaee8b03da98c3
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-07-24 02:22:26 +0300

    Add PHProtobufModels target and Protobuf models
    Former-commit-id: ee3b44f6ba7266476f795bc4dfb5ccb547fdbb94

---
+++ PHProtobufModels/Supporting Files/PHProtobufModels.h
@@ -0,0 +1,19 @@
+//
+//  PHProtobufModels.h
+//  PHProtobufModels
+//
+//  Created by Dmitry Fa[n]tastik on 24/07/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHProtobufModels.
+FOUNDATION_EXPORT double PHProtobufModelsVersionNumber;
+
+//! Project version string for PHProtobufModels.
+FOUNDATION_EXPORT const unsigned char PHProtobufModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHProtobufModels/PublicHeader.h>
+
+
