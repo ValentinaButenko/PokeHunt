7f52b239e5b77dd0c5465b5dd75b83b30da09496
Valentina <v.o.butenko@gmail.com>
2016-07-24 14:02:08 +0300

    Move files between directories
    Former-commit-id: 387821f89c179af2050c064d31522ba40b3e4ca3

---
+++ PokeHunt/Classes/ViewControllers/SettingsTableVC.swift
@@ -0,0 +1,50 @@
+//
+//  SettingsTableVC.swift
+//  PokeHunt
+//
+//  Created by Valentina Butenko on 7/23/16.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class SettingsTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 9
+    }
+
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect.zero)
+        let headerText = UILabel()
+
+        headerText.text = "Settings"
+        headerText.font = UIFont(name: "OpenSans-Semibold", size: 14)
+
+        headerText.textColor = UIColor(red: (124/255), green: (124/255), blue: (124/255), alpha: 1.0)
+        headerView.backgroundColor = UIColor(red: (243/255), green: (243/255), blue: (243/255), alpha: 1.0)
+
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.addSubview(headerText)
+
+        headerText.snp_makeConstraints { (make) in
+            make.top.equalTo(headerView).inset(8)
+            make.leading.equalTo(headerView).inset(15)
+        }
+        return headerView
+    }
+
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 37
+    }
+
+   }
