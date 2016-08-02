20cedf930d2c7d2b2ec46a01a4e8d42c35640c30
Valentina <v.o.butenko@gmail.com>
2016-08-02 12:10:58 +0300

    Create PrivacyPolicy view
    Former-commit-id: 7a8ead2c34164bee4cb3c65d47881becb99fa85f

--- PokeHunt/Classes/ViewControllers/LoginVC.swift
+++ PokeHunt/Classes/ViewControllers/LoginVC.swift
@@ -16,11 +16,11 @@ class LoginVC: UIViewController {
     var logoView: LogoView!
     var loginBtn: UIButton!
     var warningLbl: UILabel!
+    var privacyView: PrivacyPolicyView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setup()
-//        Flurry.logPageView()
 
         view.backgroundColor = UIColor.whiteColor()
     }
@@ -28,13 +28,23 @@ class LoginVC: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
-        self.logoViewWillMove()
+     //   self.logoViewWillMove()
     }
 
     func setup(){
-        self.setupLogoView()
-        self.setupLoginBtn()
-        self.setupWarningLbl()
+        self.setupPrivacyView()
+
+
+
+//        self.setupLogoView()
+//        self.setupLoginBtn()
+//        self.setupWarningLbl()
+    }
+
+    func setupPrivacyView(){
+        let privacyView = PrivacyPolicyView(frame: CGRect(x: 40, y: 70, width: self.view.frame.size.width - 80, height: self.view.frame.size.height - 140))
+        view.addSubview(privacyView)
+        self.privacyView = privacyView
     }
 
     func setupLogoView(){
@@ -53,7 +63,7 @@ class LoginVC: UIViewController {
         loginBtn.setImage(UIImage(named: R.image.gmail.name), forState: .Normal)
         loginBtn.imageView?.contentMode = .ScaleAspectFit
         loginBtn.alpha = 0.0
-        loginBtn.addTarget(self, action: #selector(LoginVC.tappedLoginBtn(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+  //      loginBtn.addTarget(self, action: #selector(LoginVC.tappedLoginBtn(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         view.addSubview(loginBtn)
 
         loginBtn.snp_makeConstraints { (make) in
@@ -84,57 +94,57 @@ class LoginVC: UIViewController {
         self.warningLbl = warningLbl
     }
 
-    func logoViewWillMove(){
-        self.logoView.snp_updateConstraints(closure: { (make) in
-            make.centerY.equalTo(self.view).offset(-80)
-        })
+//    func logoViewWillMove(){
+//        self.logoView.snp_updateConstraints(closure: { (make) in
+//            make.centerY.equalTo(self.view).offset(-80)
+//        })
+//
+//        let animation = {
+//            self.view.layoutIfNeeded()
+//        }
+//
+//        UIView.animateWithDuration(0.45,
+//                                   delay: 0.5,
+//                                   options: UIViewAnimationOptions.CurveEaseIn,
+//                                   animations: animation,
+//                                   completion: { _ in self.loginBtnWillAppear() })
+//    }
 
-        let animation = {
-            self.view.layoutIfNeeded()
-        }
+//    func loginBtnWillAppear(){
+//        self.loginBtn.snp_updateConstraints(closure: { (make) in
+//            make.top.equalTo(self.logoView.snp_bottom).inset(-10)
+//        })
+//
+//        let animation = {
+//            self.loginBtn.alpha = 1.0
+//            self.view.layoutIfNeeded()
+//        }
+//        UIView.animateWithDuration(0.45,
+//                                   animations: animation,
+//                                   completion: {_ in self.warningLblWillAppear()})
+//    }
 
-        UIView.animateWithDuration(0.45,
-                                   delay: 0.5,
-                                   options: UIViewAnimationOptions.CurveEaseIn,
-                                   animations: animation,
-                                   completion: { _ in self.loginBtnWillAppear() })
-    }
+//    func warningLblWillAppear(){
+//        let animation = {
+//            self.warningLbl.alpha = 1.0
+//            self.view.layoutIfNeeded()
+//        }
+//        UIView.animateWithDuration(0.5,
+//                                   animations: animation,
+//                                   completion: nil)
+//    }
 
-    func loginBtnWillAppear(){
-        self.loginBtn.snp_updateConstraints(closure: { (make) in
-            make.top.equalTo(self.logoView.snp_bottom).inset(-10)
-        })
-
-        let animation = {
-            self.loginBtn.alpha = 1.0
-            self.view.layoutIfNeeded()
-        }
-        UIView.animateWithDuration(0.45,
-                                   animations: animation,
-                                   completion: {_ in self.warningLblWillAppear()})
-    }
-
-    func warningLblWillAppear(){
-        let animation = {
-            self.warningLbl.alpha = 1.0
-            self.view.layoutIfNeeded()
-        }
-        UIView.animateWithDuration(0.5,
-                                   animations: animation,
-                                   completion: nil)
-    }
-
-    func tappedLoginBtn(sender: UIButton!){
-        FIRAnalytics.logEventWithName("User_log_in", parameters: nil)
-        LoginModule.sharedModule.performLoginOnController(self) { res in
-            switch (res) {
-            case .LoggedIn:
-                let vc = MapVC()
-                Bootstrapper.exchangeRoot(viewController: UINavigationController(rootViewController: vc))
-            case .Failed:
-                print("failed")
-            }
-        }
-    }
+//    func tappedLoginBtn(sender: UIButton!){
+//        FIRAnalytics.logEventWithName("User_log_in", parameters: nil)
+//        LoginModule.sharedModule.performLoginOnController(self) { res in
+//            switch (res) {
+//            case .LoggedIn:
+//                let vc = MapVC()
+//                Bootstrapper.exchangeRoot(viewController: UINavigationController(rootViewController: vc))
+//            case .Failed:
+//                print("failed")
+//            }
+//        }
+//    }
 }
 
---
+++ PokeHunt/Classes/Views/PrivacyPolicyView.swift
@@ -0,0 +1,99 @@
+//
+//  PrivacyPolicyView.swift
+//  PokeHunt
+//
+//  Created by Valentina Butenko on 8/2/16.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PrivacyPolicyView: UIView{
+
+    var textView: UITextView!
+    var declineBtn: UIButton!
+    var comfirmBtn: UIButton!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        self.setup()
+    }
+
+    func setup(){
+        self.setupTextView()
+        self.showTextToView()
+
+        self.setupDeclineBtn()
+        self.setupConfirmBtn()
+    }
+
+    func setupTextView(){
+        let textView = UITextView(frame: CGRect(x: 0,
+                                                y: 0,
+                                                width: self.frame.size.width,
+                                                height: self.frame.size.height - 35))
+        textView.scrollEnabled = true
+        textView.backgroundColor = UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 1.0)
+        textView.editable = false
+        textView.font = UIFont(name: "OpenSans", size: 12)
+        textView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        textView.textColor = UIColor(red: 28/255, green: 28/255, blue: 28/255, alpha: 1.0)
+
+        self.addSubview(textView)
+        self.textView = textView
+    }
+
+    func setupDeclineBtn(){
+        let declineBtn = UIButton(frame: CGRect(x: 0,
+                                                y: self.textView.frame.size.height + 5,
+                                                width: self.frame.size.width/2 - 2.5,
+                                                height: 30))
+        declineBtn.enabled = true
+        declineBtn.backgroundColor = UIColor(red: 214/255, green: 69/255, blue: 65/255, alpha: 1.0)
+        declineBtn.setTitle("Decline", forState: .Normal)
+        declineBtn.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        declineBtn.titleLabel?.font = UIFont(name: "OpenSans", size: 14)
+
+        self .addSubview(declineBtn)
+        self.declineBtn = declineBtn
+    }
+
+    func setupConfirmBtn(){
+        let confirmBtn = UIButton(frame: CGRect(x: self.declineBtn.frame.size.width + 5,
+                                                y: self.textView.frame.size.height + 5,
+                                                width: self.frame.width/2 - 2.5,
+                                                height: 30))
+        declineBtn.enabled = true
+
+        confirmBtn.backgroundColor = UIColor(red: 3/255, green: 201/255, blue: 169/255, alpha: 1.0)
+        confirmBtn.setTitle("Confirm", forState: .Normal)
+        confirmBtn.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        confirmBtn.titleLabel?.font = UIFont(name: "OpenSans", size: 14)
+
+        self .addSubview(confirmBtn)
+        self.comfirmBtn = confirmBtn
+    }
+
+
+    func showTextToView(){
+        if let txtURL = NSBundle.mainBundle().URLForResource("privacy", withExtension: "txt"){
+            do{
+                let text = try NSString(contentsOfURL: txtURL, encoding: NSUTF8StringEncoding)
+                textView.text = text as String
+            }
+            catch{
+                print(NSError.description())
+            }
+        }else{
+            print(NSError.description())
+        }
+    }
+}
