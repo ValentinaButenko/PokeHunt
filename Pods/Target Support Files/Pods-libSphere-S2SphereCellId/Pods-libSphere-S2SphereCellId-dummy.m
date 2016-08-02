29a8753f3bead945b88420b09aae1eb120c15776
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-08-02 20:32:04 +0300

    Add S2SphereAdapter target
    Former-commit-id: 7f2a20ae5335200de0b3f1f4cf79608b0f1e0830

---
+++ Pods/Target Support Files/Pods-libSphere-S2SphereCellId/Pods-libSphere-S2SphereCellId-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_libSphere_S2SphereCellId : NSObject
+@end
+@implementation PodsDummy_Pods_libSphere_S2SphereCellId
+@end
---
+++ S2SphereAdapter_/Supporting Files/S2SphereAdapter.h
@@ -0,0 +1,20 @@
+//
+//  S2SphereAdapter.h
+//  S2SphereAdapter
+//
+//  Created by Dmitry Fa[n]tastik on 26/07/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "S2SphereCellIdFactory.h"
+
+//! Project version number for S2SphereAdapter.
+FOUNDATION_EXPORT double S2SphereAdapterVersionNumber;
+
+//! Project version string for S2SphereAdapter.
+FOUNDATION_EXPORT const unsigned char S2SphereAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <S2SphereAdapter/PublicHeader.h>
+
+
